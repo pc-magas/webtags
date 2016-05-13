2505fd5e42b4d53e7d26d9dff528ee0cfda26583
pcmagas <pc_magas@yahoo.gr>
2016-05-13 21:51:22 +0300

    Display An Input form

--- application/views/display_hashtag.php
+++ application/views/display_hashtag.php
@@ -2,10 +2,23 @@
 <html>
 <head>
   <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Generated Hashtags</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
 </head>
 <body>
+  <h1>Generated Tags</h1>
 <?php
-  var_dump($tags);
+  foreach($tags as $t)
+  {
+    echo "<span class=\"label label-default\">#$t</span>&nbsp;";
+  }
 ?>
 </body>
 </html>
